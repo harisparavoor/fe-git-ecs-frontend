c16bb318b91c4ca421737d77707e1c88949f1595
Hari P S <hari.ps@experionglobal.local>
2026-09-16 09:50:14 +0530

    update

---
+++ frontend/src/authConfig.ts
@@ -0,0 +1,8 @@
+const envVariables = {
+  clientId: "$CLIENTID",
+  authority: "$AD_AUTHORITY",
+  apiUrl: "$APP_API_URL",
+  apiScope: "$AD_API_SCOPE",
+  chatOrigin: "$CHAT_ORIGIN",
+  notesOrigin: "$NOTES_ORIGIN",
+};
